8b7b19c80823e80a2164e110c04dd99fdcf51d3f
Scott Court <scott.c.court@gmail.com>
2021-12-06 13:25:26 -0500

    Added a duckduckgo search.

---
+++ duckDuckGoSearch.py
@@ -0,0 +1,14 @@
+#! python3
+#! duckDuckGoSearch.py - Launches a DDG search from the command line or clipboard
+#! Search for "hello" == https://duckduckgo.com/?q=hello&va=b&t=hr&ia=web
+
+import webbrowser, sys, pyperclip
+
+if len(sys.argv) > 1:
+    # Get address from command line.
+    search = ' '.join(sys.argv[1:])
+else:
+    # Get address for clipboard.
+    search = pyperclip.paste()
+
+webbrowser.open('https://duckduckgo.com/?q=' + search)
