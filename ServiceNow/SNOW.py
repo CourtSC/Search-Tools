5a8885d7f0bd742d4eef10823037be5e2a021db5
Court <SCO941@multihosp.net>
2024-06-05 09:32:27 -0400

    modified SNOW.py to search active tickets for keyword.

--- ServiceNow/SNOW.py
+++ ServiceNow/SNOW.py
@@ -6,9 +6,11 @@ import sys, webbrowser, pyperclip
 
 if len(sys.argv) > 1:
     # Get search from args.
-    search = ' '.join(sys.argv[1:])
+    search = " ".join(sys.argv[1:])
 else:
     # Get search from clipboard.
     search = pyperclip.paste()
 
-webbrowser.open('https://servicenow.adventhealth.com/nav_to.do?uri=%2F$sn_global_search_results.do%3Fsysparm_search%3D' + search)
+webbrowser.open(
+    f"https://servicenow.adventhealth.com/now/nav/ui/classic/params/target/incident_list.do%3Fsysparm_query%3DGOTO123TEXTQUERY321%253D{search}%255Eactive%253Dtrue%26sysparm_first_row%3D1%26sysparm_view%3D"
+)
